fe276b526209e0b21826e5eb1d1d34e2eeb39dc5
naveen alka <naveenalka@naveens-MacBook-Pro.local>
2018-07-10 00:35:03 -0500

    fix structure

--- seating/src/main/java/com/theater/seating/service/TicketService.java
+++
@@ -1,9 +0,0 @@
-package com.theater.seating.service;
-
-import com.theater.seating.domain.Ticket;
-
-public interface TicketService {
-
-	public Ticket bookATicket(String userName,Integer noOfTickets);
-	public void showMeTheater();
-}
